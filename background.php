01de51a79f9bc9def55e44083dbc4fe6b8d2e8e2
Signofwish <ygkim@plus21kr.com>
2015-05-02 15:42:38 +0900

    150502

---
+++ background.php
@@ -0,0 +1,81 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="ko">
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>Document</title>
+    <style type="text/css">
+        body {
+            background: url("cat.jpg") no-repeat;
+            background-size: cover;
+        }
+
+        .index-notice {
+            box-sizing: border-box;
+            width: 400px;
+            height: 370px;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            margin-left: -200px;
+            margin-top: -185px;
+            background-color: rgba(255,255,255,0.85);
+            /*background-image: url('black-85.png');*/
+            padding: 2em;
+            /*color: white;*/
+            color: #000;
+            border-radius: 1em;
+            -webkit-box-shadow: 2px 2px 15px 0px rgba(54, 50, 50, 0.77);
+            -moz-box-shadow:    2px 2px 15px 0px rgba(54, 50, 50, 0.77);
+            box-shadow:         2px 2px 15px 0px rgba(54, 50, 50, 0.77);
+        }
+        .index-notice a {
+            /*color: white;*/
+            color: #000;
+            text-decoration: none;
+        }
+        .index-notice h3 {
+            margin: 0;
+        }
+    </style>
+</head>
+<body>
+    <div class="division3  index-notice">
+        <hgroup>
+            <h3>휴온스 <span>소식</span></h3>
+            <span></span>
+        </hgroup>
+        <ul>
+            <li>
+                <a href="javascript:return false;">
+                    중국 피부미용 의료기기 인기로 수출물량 2.5배↑ </a>
+                <span>2015.04.08</span>
+            </li>
+            <li>
+                <a href="javascript:return false;">
+                    세계판매1위 금연보조제 니코레트 판매 </a>
+                <span>2015.04.08</span>
+            </li>
+            <li>
+                <a href="javascript:return false;">
+                    중국공장 가동으로 글로벌 헬스케어 그룹 박차 </a>
+                <span>2015.03.27</span>
+            </li>
+            <li>
+                <a href="javascript:return false;">
+                    패혈증 신약개발 임상2상 승인 </a>
+                <span>2015.03.27</span>
+            </li>
+            <li>
+                <a href="javascript:return false;">
+                    자동주입 가능한 엘라비에 밸런스 필러 독점 판매 </a>
+                <span>2015.03.27</span>
+            </li>
+            <li>
+                <a href="javascript:return false;">
+                    휴메딕스, 중국시장 진출 통한 매출 극대화 기대 </a>
+                <span>2015.03.27</span>
+            </li>
+        </ul>
+    </div>
+</body>
+</html>
--- box-model.php
+++ box-model.php
@@ -5,46 +5,94 @@
     <title></title>
     <style>
         body {
-            width: 980px;
             font-family: 'Apple SD Gothic Neo', '애플 SD 고딕 Neo', 'NanumBarunGothic', 'NanumBarunGothicOTF', '나눔바른고딕', '나눔바른고딕OTF', 'Malgun Gothic', '맑은 고딕', 'NanumGothic', 'NanumGothicOTF', '나눔고딕', '나눔고딕OTF', sans-serif;
-        }
-        .division3 {
-            width: 33%;
-            float: left;
-            margin: 0 20px 0 20px;
-        }
-        .division3 > hgroup {
-            font-size: 20px;
-        }
-
-        .division3 .content {
-            background-color: #eeeeee;
             font-size: 14px;
             line-height: 1.6;
-            padding: 20px 20px 20px 40px;
-            margin-top: 20px;
-            border: 1px solid #dddddd;
         }
-        .division3:last-child {
-             
-        }
-
         a {
             text-decoration: none;
             color: #000000;
         }
+        ul {
+            margin-top: 0;
+            margin-bottom: 30px;
+        }
+
+        .grid:after, .cf:after {
+            content: '';
+            display: block;
+            clear: both;
+        }
+
+        .l-wrapper {
+            max-width: 980px;
+            margin: 0 auto;
+        }
+        .division3 {
+            box-sizing: border-box;
+            width: 33.333333333333333%;
+            float: left;
+        }
+
+        .index-notice {
+            padding: 30px 30px 0;
+            border-right: 1px dotted #dddddd;
+        }
+        .grid__item.index-notice {
+            padding: 30px 0 0 30px;
+            border-right: 0;
+        }
+        .index-notice > hgroup {
+            font-size: 1.5em;
+            margin-bottom: 20px;
+        }
+
+        .index-notice > ul {
+            background-color: #eeeeee;
+            padding: 20px 20px 20px 40px;
+        }
+        .index-notice:last-child {
+            border-right: 0;
+        }
+        @media screen and (max-width: 765px) {
+            .division3 {
+                width: auto;
+                float: none;
+            }
+        }
+
+        .tempbox {
+            clear: both;
+            background-color: #eee;
+            padding: 2em;
+        }
+
+
+        .grid {
+            margin-left: -30px;
+            clear: both;
+        }
+        .grid__item {
+            float: left;
+            box-sizing: border-box;
+        }
+        .grid-1-3 {
+            width: 33.333333333333333%
+        }
     </style>
 </head>
 <body>
+<div class="l-wrapper cf">
+
     <?php
     $i = 0;
     while($i++<3){ ?>
-        <div class="division3  <?= ( $i == 3 ) ? 'division3--last' : '' ?>">
+        <div class="division3  index-notice">
             <hgroup>
                 <h3>휴온스 <span>소식</span></h3>
                 <span></span>
             </hgroup>
-            <ul class="content">
+            <ul>
                 <li>
                     <a href="javascript:return false;">
                         중국 피부미용 의료기기 인기로 수출물량 2.5배↑ </a>
@@ -79,6 +127,103 @@
         </div>
     <?php } ?>
 
+    <div class="grid">
+        <?php
+        $i = 0;
+        while($i++<3){ ?>
+        <div class="grid__item  grid-1-3  index-notice">
+            <hgroup>
+                <h3>휴온스 <span>소식</span></h3>
+                <span></span>
+            </hgroup>
+            <ul>
+                <li>
+                    <a href="javascript:return false;">
+                        중국 피부미용 의료기기 인기로 수출물량 2.5배↑ </a>
+                    <span>2015.04.08</span>
+                </li>
+                <li>
+                    <a href="javascript:return false;">
+                        세계판매1위 금연보조제 니코레트 판매 </a>
+                    <span>2015.04.08</span>
+                </li>
+                <li>
+                    <a href="javascript:return false;">
+                        중국공장 가동으로 글로벌 헬스케어 그룹 박차 </a>
+                    <span>2015.03.27</span>
+                </li>
+                <li>
+                    <a href="javascript:return false;">
+                        패혈증 신약개발 임상2상 승인 </a>
+                    <span>2015.03.27</span>
+                </li>
+                <li>
+                    <a href="javascript:return false;">
+                        자동주입 가능한 엘라비에 밸런스 필러 독점 판매 </a>
+                    <span>2015.03.27</span>
+                </li>
+                <li>
+                    <a href="javascript:return false;">
+                        휴메딕스, 중국시장 진출 통한 매출 극대화 기대 </a>
+                    <span>2015.03.27</span>
+                </li>
+            </ul>
+        </div>
+        <?php } ?>
+    </div>
 
+    <div class="tempbox">
+
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+        휴메딕스, 중국시장 진출 통한 매출 극대화 기대
+    </div>
+
+</div>
 </body>
 </html>
